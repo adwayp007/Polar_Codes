0483969e348a37a456bfd32e5bb04b1ad3aa5d90
adwayp007 <68924769+adwayp007@users.noreply.github.com>
2023-11-29 15:39:51 -0500

    Update master_polar.m

--- master_polar.m
+++ master_polar.m
@@ -1,18 +1,13 @@
-function [list] = master_polar(A,n)
-
-%%% THE MASTER FUNCTION FOR THE POLAR ENCODER
-%%% INPUT: CHANNEL TRANSITION MATRIX A
-%%%        SIZE PARAMETER n
+function list = master_polar(A, n)
+%% Master function for the polar encoder
+%% Input: Channel transition matrix A, size parameter n
 
 N = 2^n;
+list = zeros(N, 1);
 
-list = zeros(N,1);
-
-for i=1:N
-    
-    list(i,1) = polar_channel_probs(A,n,i-1);
-    disp(list(i,1));
+for i = 1:N
+    list(i) = polar_channel_probs(A, n, i - 1);
+    disp(list(i));
 end
 
 end
-
